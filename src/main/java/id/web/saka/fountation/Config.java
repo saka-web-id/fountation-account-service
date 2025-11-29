06097de6c4c587b624777417b34e2c1dca59d331
sakawijaya <sakawijaya@gmail.com>
2025-11-29 16:06:03 +0700

    Eureka Configuration

--- src/main/java/id/web/saka/fountation/Config.java
+++ src/main/java/id/web/saka/fountation/Config.java
@@ -3,19 +3,20 @@ package id.web.saka.fountation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class Config {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                        authorize -> authorize.anyRequest().authenticated())
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(auth -> auth.pathMatchers("/health").permitAll())
+                .authorizeExchange(auth -> auth.anyExchange().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
         return http.build();
     }
 
--- src/main/java/id/web/saka/fountation/account/Account.java
+++ src/main/java/id/web/saka/fountation/account/Account.java
@@ -1,20 +1,3 @@
 package id.web.saka.fountation.account;
 
-public class Account {
-
-    private String id;
-    private String name;
-
-    public Account(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-}
+public record Account(String id, String name) { }
