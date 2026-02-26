4bb36d0956acbd3fafca99ce883288b2c16fe939
sakawijaya <sakawijaya@gmail.com>
2026-02-26 11:38:30 +0700

    Remove Cookie Manage and Edit Dockerfile. 20260226

--- src/main/java/id/web/saka/fountation/config/Config.java
+++ src/main/java/id/web/saka/fountation/config/Config.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -14,9 +15,17 @@ public class Config {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-                .authorizeExchange(auth -> auth.pathMatchers("/api/v0/account/health").permitAll())
-                .authorizeExchange(auth -> auth.pathMatchers("/api/v0/account/user/registration").permitAll())
-                .authorizeExchange(auth -> auth.pathMatchers("/api/v0/account/membership/detail/**").hasAuthority("SCOPE_internal:service"))
+                // 1. Force stateless session management
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .authorizeExchange(auth ->
+                        auth.pathMatchers(
+                                "/api/v0/account/health",
+                                "/api/v0/account/user/registration"
+                        ).permitAll())
+                .authorizeExchange(
+                        auth -> auth.pathMatchers(
+                                "/api/v0/account/membership/detail/**"
+                        ).hasAuthority("SCOPE_internal:service"))
                 .authorizeExchange(auth -> auth.anyExchange().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
